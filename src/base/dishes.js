e4a579fcd8860b35ea88e42393ad4535451380f9
Mars-Lucifer <89739852+Mars-Lucifer@users.noreply.github.com>
2023-06-23 15:51:53 +0300

    on/off

--- src/base/dishes.js
+++ src/base/dishes.js
@@ -5,6 +5,7 @@ import Header from './header';
 import { recList } from './recipes';
 
 const Dishes = () => {
+    //получение списка
     const { id } = useParams();
     const dish = recList.find((dish) => dish.id === parseInt(id));
 
@@ -12,6 +13,18 @@ const Dishes = () => {
         const img = document.getElementById('img');
         img.style.backgroundImage = `url(${dish.img})`;
     }, [dish]);
+
+    //Функция развертки
+    useEffect(() => {
+        const butt = document.getElementById("button_i");
+
+        butt.addEventListener('click', (() => {
+            const content = document.getElementById("dish_info");
+
+            content.classList.toggle('on');
+            content.classList.toggle('off');
+        }));
+    })
 
     return (
         <div className='row'>
@@ -38,6 +51,15 @@ const Dishes = () => {
                 <div className='el__dish ed_3'>
                     <p>Fats</p>
                     <p className='text__el ed_3'>{dish.fats}</p>
+                </div>
+            </div>
+
+            <div className='col-8'>
+                <div className='rec_info off' id="dish_info">
+                    <div className='box_rec_info'>
+                        <p className='rec_big'>{dish.cooking_header}</p>
+                        <button className='img__block_1__button free' id="button_i"></button>
+                    </div>
                 </div>
             </div>
         </div>
